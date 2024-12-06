b53d744ac86fb60e753b5e8b92bbc0cdca83994b
M6K4N9R <zubchevskiy@gmail.com>
2024-12-06 18:03:05 +0100

    Add 1-intro file

--- 1-intro.js
+++ 1-intro.js
@@ -1,11 +1,9 @@
 const amount = 9;
 
 if (amount < 10) {
-    console.log("small number");
-    
+  console.log("small number");
 } else {
-    console.log("large number");
-    
+  console.log("large number");
 }
 
-console.log("Test");
+// console.log("Test");
---
+++ app.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+    console.log("Hey Andrii");
+  }, 2000);
+  
