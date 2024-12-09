4265013a859f8489c6384cf1457f410f99415a3f
M6K4N9R <zubchevskiy@gmail.com>
2024-12-09 18:47:42 +0100

    Play with asyncronous and server port

--- app.js
+++ app.js
@@ -7,17 +7,45 @@
 
 // console.log("next time");
 
-const { readFile } = require("fs");
+// const { readFile } = require("fs");
 
-console.log("started a first task");
+// console.log("started a first task");
 
-readFile("./content/first.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(result);
-  console.log("complete first task");
-});
+// readFile("./content/first.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(result);
+//   console.log("complete first task");
+// });
 
-console.log("starting second task");
+// console.log("starting second task")
+
+// asyncronous
+// console.log('first');
+// setTimeout(() => {
+//     console.log('second');
+    
+// }, 0)
+// console.log('third');
+
+// setInterval(() => {
+//     console.log('hello world');
+    
+// }, 2000)
+// console.log('I will run first');
+
+
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('request event');
+    res.end('Yo man')
+    
+})
+
+server.listen(5000, () => {
+    console.log('Server listening on port : 5000....');
+    
+})
