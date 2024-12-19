1eaec8a51d27c0eb7e632842dffcf1409f445c87
M6K4N9R <zubchevskiy@gmail.com>
2024-12-19 14:32:46 +0100

    Create server

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,8 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("Hey there");
+  // console.log(req.method);
+  const url = req.url;
+  // home page
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+  }
+  // about page
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+  }
+  // 404
+  else {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Page Not Found</h1>");
+    res.end();
+  }
 });
 
 server.listen(5000);
